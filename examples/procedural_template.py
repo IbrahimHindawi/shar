8810a206fb14a7e1defaba876b147cc70f158b2b
IbrahimHindawi <ihindawi@gmail.com>
2024-02-03 18:53:32 +0400

    edited proc_script

--- examples/procedural_template.py
+++ examples/procedural_template.py
@@ -3,61 +3,59 @@ import importlib
 import shar
 importlib.reload(shar)
 
-character_name = "charac"
+character_name = 'charac'
 
-def get_node(character_name, node_name):
-    return hou.node('/obj/'+character_name+'/'+node_name)
+def import_character(character_name):
+    hou.hipFile.merge(hou.expandString('$' + 'HIP')+ '/' + character_name + '.hiplc')
 
-def import_model():
-    path = "$" + "HIP"
-    expanded = hou.expandString(path)
-    hou.hipFile.merge(expanded + '/' + character_name + ".hiplc")
+def get_node(rig, node_name):
+    return hou.node(rig.path()+'/'+node_name)
 
-def execute_procedural_rigging_process():
-    rig = hou.node('/obj/'+character_name)
+def execute_procedural_rigging_process(rig):
     L_arm = [ 
-        get_node(character_name, 'L_shoulder_bone0'),
-        get_node(character_name, 'L_arm_bone0'),
-        get_node(character_name, 'L_forearm_bone0'),
-        get_node(character_name, 'L_hand_bone0'),
-        get_node(character_name, 'L_twist_bone0')
+        get_node(rig, 'L_shoulder_bone0'),
+        get_node(rig, 'L_arm_bone0'),
+        get_node(rig, 'L_forearm_bone0'),
+        get_node(rig, 'L_hand_bone0'),
+        get_node(rig, 'L_twist_bone0')
     ]
 
     R_arm = [ 
-        get_node(character_name, 'R_shoulder_bone0'),
-        get_node(character_name, 'R_arm_bone0'),
-        get_node(character_name, 'R_forearm_bone0'),
-        get_node(character_name, 'R_hand_bone0'),
-        get_node(character_name, 'R_twist_bone0')
+        get_node(rig, 'R_shoulder_bone0'),
+        get_node(rig, 'R_arm_bone0'),
+        get_node(rig, 'R_forearm_bone0'),
+        get_node(rig, 'R_hand_bone0'),
+        get_node(rig, 'R_twist_bone0')
     ]
 
     L_leg = [ 
-        get_node(character_name, 'L_thigh_bone0'),
-        get_node(character_name, 'L_shin_bone0'),
-        get_node(character_name, 'L_foot_bone0'),
-        get_node(character_name, 'L_toe_bone0'),
-        get_node(character_name, 'L_legtwist_bone0')
+        get_node(rig, 'L_thigh_bone0'),
+        get_node(rig, 'L_shin_bone0'),
+        get_node(rig, 'L_foot_bone0'),
+        get_node(rig, 'L_toe_bone0'),
+        get_node(rig, 'L_legtwist_bone0')
     ]
 
     R_leg = [ 
-        get_node(character_name, 'R_thigh_bone0'),
-        get_node(character_name, 'R_shin_bone0'),
-        get_node(character_name, 'R_foot_bone0'),
-        get_node(character_name, 'R_toe_bone0'),
-        get_node(character_name, 'R_legtwist_bone0')
+        get_node(rig, 'R_thigh_bone0'),
+        get_node(rig, 'R_shin_bone0'),
+        get_node(rig, 'R_foot_bone0'),
+        get_node(rig, 'R_toe_bone0'),
+        get_node(rig, 'R_legtwist_bone0')
     ]
 
     head = [ 
-        get_node(character_name, 'neck_bone0'),
-        get_node(character_name, 'head_bone0'),
+        get_node(rig, 'neck_bone0'),
+        get_node(rig, 'head_bone0'),
     ]
 
     spines = [
-        get_node(character_name, 'spine_bone0'),
-        get_node(character_name, 'spine_bone1'),
-        get_node(character_name, 'spine_bone2'),
-        get_node(character_name, 'spine_bone3'),
-        get_node(character_name, 'spine_bone4'),
+        get_node(rig, 'spine_bone0'),
+        get_node(rig, 'spine_bone1'),
+        get_node(rig, 'spine_bone2'),
+        get_node(rig, 'spine_bone3'),
+        get_node(rig, 'spine_bone4'),
+        # get_node(rig, 'hip_bone0'),
     ]
 
     shar.build.initialize(rig)
@@ -66,14 +64,14 @@ def execute_procedural_rigging_process():
 
     shar.build.createSpine(rig, spines)
     
-    shar.build.createArm(rig, L_arm, "arm", shar.color.red)
-    shar.build.createFingers(rig, get_node(character_name, 'L_hand_bone0'), "hand", shar.color.red)
+    shar.build.createArm(rig, L_arm, 'arm', shar.color.red)
+    shar.build.createFingers(rig, get_node(rig, 'L_hand_bone0'), 'hand', shar.color.red)
 
-    shar.build.createArm(rig, R_arm, "arm", shar.color.blue)
-    shar.build.createFingers(rig, get_node(character_name, 'R_hand_bone0'), "hand", shar.color.blue)
+    shar.build.createArm(rig, R_arm, 'arm', shar.color.blue)
+    shar.build.createFingers(rig, get_node(rig, 'R_hand_bone0'), 'hand', shar.color.blue)
     
-    shar.build.createLeg(rig, L_leg, "leg", shar.color.red)
-    shar.build.createLeg(rig, R_leg, "leg", shar.color.blue)
+    shar.build.createLeg(rig, L_leg, 'leg', shar.color.red)
+    shar.build.createLeg(rig, R_leg, 'leg', shar.color.blue)
     
-import_model()
-execute_procedural_rigging_process()
+import_character(character_name)
+execute_procedural_rigging_process(hou.node('/obj/'+character_name))
