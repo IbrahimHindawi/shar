364784b7d852f51d05fd95a8b263deb00162cbb6
IbrahimHindawi <ihindawi@gmail.com>
2024-02-07 21:31:55 +0400

    moved utilities module logic

--- python3.10libs/shar/uiScript/sharRiggingToolkit.py
+++ python3.10libs/shar/uiScript/sharRiggingToolkit.py
@@ -154,10 +154,36 @@ class ibraRiggingToolkit(QtWidgets.QWidget):
         self.executeCreatePiston(rig)
 
     def mirrorLimbButtonClicked(self):
-        utilities.mirrorBones()
+        # shar.utilities.mirrorBones()
+        nodes = hou.selectedNodes()
+
+        for node in nodes:
+            node.movePreTransformIntoParmTransform()
+
+        for node in nodes:
+            y = node.parm("ry").eval() * -1
+            z = node.parm("rz").eval() * -1
+
+            node.parm("ry").set(y)
+            node.parm("rz").set(z)
+
+        for node in nodes:
+            node.moveParmTransformIntoPreTransform()
 
     def mirrorFingersButtonClicked(self):
-        utilities.mirrorFingers()
+        # shar.utilities.mirrorFingers()
+        nodes = hou.selectedNodes()
+
+        for node in nodes:
+            node.movePreTransformIntoParmTransform()
+
+        for node in nodes:
+            x = node.parm("tx").eval() * -1
+            
+            node.parm("tx").set(x)
+
+        for node in nodes:
+            node.moveParmTransformIntoPreTransform()
 
 
     def CreateHandsFingersButton(self):
